3028713b10874dda6697c2b69593bc5a2f2be0e5
aslanovgx <aslanov7509@gmail.com>
2023-02-19 23:26:28 +0400

    Isotope is written

--- classic/src/Components/Pages/Classic/Isotope/Isotope.jsx
+++ classic/src/Components/Pages/Classic/Isotope/Isotope.jsx
@@ -2,6 +2,288 @@ import React from "react";
 import "./Isotope.scss";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 const Isotope = () => {
+  // // state for storing the isotope object, with an initial value of null
+  // const [isotope, setIsotope] = React.useState(null);
+  // // state for storing the filter keyword, with an initial value of *, which matches everything
+  // const [filterKey, setFilterKey] = React.useState('*');
+
+  function Click_All() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "auto";
+    Small_1.style.width = "380px";
+    Small_1.style.padding = "0 12px";
+    Small_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_2.style.height = "auto";
+    Small_2.style.width = "380px";
+    Small_2.style.padding = "0 12px";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "auto";
+    Small_3.style.width = "380px";
+    Small_3.style.padding = "0 12px";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "auto";
+    Small_4.style.width = "380px";
+    Small_4.style.padding = "0 12px";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "auto";
+    Small_5.style.width = "380px";
+    Small_5.style.padding = "0 12px";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "auto";
+    Big_1.style.width = "760px";
+    Big_1.style.padding = "0 12px";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "auto";
+    Big_2.style.width = "760x";
+    Big_2.style.padding = "0 12px";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+  function Click_Apps() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "0";
+    Small_1.style.width = "0";
+    Small_1.style.padding = "0";
+    Small_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_2.style.height = "0";
+    Small_2.style.width = "0";
+    Small_2.style.padding = "0";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "auto";
+    Small_3.style.width = "380px";
+    Small_3.style.padding = "0 12px";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "0";
+    Small_4.style.width = "0";
+    Small_4.style.padding = "0";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "0";
+    Small_5.style.width = "0";
+    Small_5.style.padding = "0";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "auto";
+    Big_2.style.width = "760px";
+    Big_2.style.padding = "0 12px";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "0";
+    Big_1.style.width = "0";
+    Big_1.style.padding = "0";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+  function Click_Branding() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "0";
+    Small_1.style.width = "0";
+    Small_1.style.padding = "0";
+    Small_1.style.transition = "";
+
+    Small_2.style.height = "auto";
+    Small_2.style.width = "380px";
+    Small_2.style.padding = "0 12px";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "0";
+    Small_3.style.width = "0";
+    Small_3.style.padding = "0";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "0";
+    Small_4.style.width = "0";
+    Small_4.style.padding = "0";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "0";
+    Small_5.style.width = "0";
+    Small_5.style.padding = "0";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "auto";
+    Big_1.style.width = "760px";
+    Big_1.style.padding = "0 12px";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "0";
+    Big_2.style.width = "0";
+    Big_2.style.padding = "0";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+  function Click_Creative() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "0";
+    Small_1.style.width = "0";
+    Small_1.style.padding = "0";
+    Small_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_2.style.height = "0";
+    Small_2.style.width = "0";
+    Small_2.style.padding = "0";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "0";
+    Small_3.style.width = "0";
+    Small_3.style.padding = "0";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "auto";
+    Small_4.style.width = "380px";
+    Small_4.style.padding = "0 12px";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "0";
+    Small_5.style.width = "0";
+    Small_5.style.padding = "0";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "0";
+    Big_1.style.width = "0";
+    Big_1.style.padding = "0";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "0";
+    Big_2.style.width = "0";
+    Big_2.style.padding = "0";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+  function Click_ID() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "auto";
+    Small_1.style.width = "380px";
+    Small_1.style.padding = "0 12px";
+    Small_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_2.style.height = "auto";
+    Small_2.style.width = "380px";
+    Small_2.style.padding = "0 12px";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "0";
+    Small_3.style.width = "0";
+    Small_3.style.padding = "0";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "auto";
+    Small_4.style.width = "380px";
+    Small_4.style.padding = "0 12px";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "auto";
+    Small_5.style.width = "380px";
+    Small_5.style.padding = "0 12px";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "auto";
+    Big_1.style.width = "760px";
+    Big_1.style.padding = "0 12px";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "0";
+    Big_2.style.width = "0";
+    Big_2.style.padding = "0";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+  function Click_Mock() {
+    const Small_1 = document.querySelector("#small_1");
+    const Small_2 = document.querySelector("#small_2");
+    const Small_3 = document.querySelector("#small_3");
+    const Small_4 = document.querySelector("#small_4");
+    const Small_5 = document.querySelector("#small_5");
+    const Big_1 = document.querySelector("#big_1");
+    const Big_2 = document.querySelector("#big_2");
+
+    // console.log(Small_1);
+
+    Small_1.style.height = "0";
+    Small_1.style.width = "0";
+    Small_1.style.padding = "0";
+    Small_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_2.style.height = "0";
+    Small_2.style.width = "0";
+    Small_2.style.padding = "0";
+    Small_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_3.style.height = "0";
+    Small_3.style.width = "0";
+    Small_3.style.padding = "0";
+    Small_3.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_4.style.height = "auto";
+    Small_4.style.width = "380px";
+    Small_4.style.padding = "0 12px";
+    Small_4.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Small_5.style.height = "auto";
+    Small_5.style.width = "380px";
+    Small_5.style.padding = "0 12px";
+    Small_5.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_1.style.height = "0";
+    Big_1.style.width = "0";
+    Big_1.style.padding = "0";
+    Big_1.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+
+    Big_2.style.height = "0";
+    Big_2.style.width = "0";
+    Big_2.style.padding = "0";
+    Big_2.style.transition = "all cubic-bezier(.645,.045,.355,1) .5s;";
+  }
+
   return (
     <section id="isotope">
       <div className="container">
@@ -9,27 +291,27 @@ const Isotope = () => {
           <div className="col-lg-12">
             <ul className="lists">
               <li id="normal_li">Filter by</li>
-              <li id="special_li">
+              <li id="special_li" className="special_li1" onClick={Click_All}>
                 <p>07</p>
                 <span>All / </span>
               </li>
-              <li id="special_li">
+              <li id="special_li" onClick={Click_Apps}>
                 <p>02</p>
                 <span>Apps / </span>
               </li>
-              <li id="special_li">
+              <li id="special_li" onClick={Click_Branding}>
                 <p>02</p>
                 <span>Branding / </span>
               </li>
-              <li id="special_li">
+              <li id="special_li" onClick={Click_Creative}>
                 <p>01</p>
                 <span>Creative / </span>
               </li>
-              <li id="special_li">
+              <li id="special_li" onClick={Click_ID}>
                 <p>05</p>
                 <span>Identity / </span>
               </li>
-              <li id="special_li">
+              <li id="special_li" onClick={Click_Mock}>
                 <p>02</p>
                 <span>Mockup /</span>
               </li>
@@ -37,7 +319,7 @@ const Isotope = () => {
           </div>
         </div>
         <div className="row row_left_right g-4 pt-5">
-          <div className="col-lg-4">
+          <div className="col-lg-4 col-md-6 col-sm-12" id="small_1">
             <div className="left_img">
               <img
                 src="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-1/zdfLZADL-oh__demo31__1.jpeg"
@@ -59,12 +341,15 @@ const Isotope = () => {
               </div>
             </div>
           </div>
-          <div className="col-lg-8">
+          <div className="col-lg-8 col-md-6 col-sm-12" id="big_1">
             <div className="right_img">
               <img
                 src="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-1/oh__img25-1920x1076.jpeg"
                 alt=""
               />
+              <div className="centre_icon">
+                <BsFillPlayCircleFill />
+              </div>
               <div className="img_details">
                 <h3>Diseño Gráfico</h3>
                 <div className="span">
@@ -78,10 +363,98 @@ const Isotope = () => {
               </div>
             </div>
           </div>
-          <div className="col-lg-4">
+          <div className="col-lg-4 col-md-6 col-sm-12" id="small_2">
             <div className="left_img">
               <img
-                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/ohio-stage-demo-1/zdfLZADL-oh__demo31__1.jpeg"
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/stage.clbthemes.com/7evLAXa6-oh__img30-1.jpeg"
+                alt=""
+              />
+              <div className="centre_icon">
+                <BsFillPlayCircleFill />
+              </div>
+              <div className="img_details">
+                <h3>Stickers Pack</h3>
+                <div className="span">
+                  <span>
+                    Identity, Mockup
+                    <div className="span2">
+                      <span>Show project</span>
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12" id="small_3">
+            <div className="left_img">
+              <img
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/stage.clbthemes.com/JwFczCN8-oh__img35.jpeg"
+                alt=""
+              />
+              <div className="centre_icon">
+                <BsFillPlayCircleFill />
+              </div>
+              <div className="img_details">
+                <h3>Stickers Pack</h3>
+                <div className="span">
+                  <span>
+                    Identity, Mockup
+                    <div className="span2">
+                      <span>Show project</span>
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12" id="small_4">
+            <div className="left_img">
+              <img
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/stage.clbthemes.com/guQWgJXg-oh__img26.jpeg"
+                alt=""
+              />
+              <div className="centre_icon">
+                <BsFillPlayCircleFill />
+              </div>
+              <div className="img_details">
+                <h3>Stickers Pack</h3>
+                <div className="span">
+                  <span>
+                    Identity, Mockup
+                    <div className="span2">
+                      <span>Show project</span>
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-8 col-md-6 col-sm-12" id="big_2">
+            <div className="right_img">
+              <img
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/stage.clbthemes.com/oh__img95-1920x1076.jpeg"
+                alt=""
+              />
+              <div className="centre_icon">
+                <BsFillPlayCircleFill />
+              </div>
+              <div className="img_details">
+                <h3>Diseño Gráfico</h3>
+                <div className="span">
+                  <span>
+                    Branding, Identity
+                    <div className="span2">
+                      <span>Show project </span>
+                    </div>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-12" id="small_5">
+            <div className="left_img">
+              <img
+                src="https://colabrio.ams3.cdn.digitaloceanspaces.com/stage.clbthemes.com/MeKCvxP5-oh__img34.1.jpeg"
                 alt=""
               />
               <div className="centre_icon">
